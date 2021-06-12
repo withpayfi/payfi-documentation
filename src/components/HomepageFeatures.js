14d744d3c114adaeb82929fbb8bd0f496c307d35
Pheranmie <tobi.okekunle@reventtechnologies.com>
2021-06-12 23:39:27 +0100

    edited homepage

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -8,8 +8,7 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Get Started with PayFI Quick.
       </>
     ),
   },
@@ -18,18 +17,17 @@ const FeatureList = [
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        We lets you focus on your business,
+        while we handle the payment process.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by PayFi',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Extend or customize your website Reusing our SDK.
       </>
     ),
   },
